702cb793f06334193023c87fd802dfa4c034269b
jiangliu322 <zhangyang3223@gmail.com>
2019-12-09 09:22:23 +0800

    贪吃蛇第一次提交

---
+++ tcsgame/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './components/pages/Home'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+  html,body {
+    width: 100%;
+    height: 100%;
+  }
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+</style>
